5f21533a8aae56dd242d8b28298df2cd30a1dbb0
Mathes <mathesk787@gmail.com>
2025-05-02 11:51:26 +0530

    Fuel cal1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from pulp import LpProblem, LpVariable, lpSum, LpMinimize
 
@@ -6,13 +6,12 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/optimize', methods=['POST'])
-def optimize_fuel():
+def optimize():
     data = request.get_json()
-
     n = data['num_machines']
     D = data['fuel_demand']
     T = data['total_fuel']
@@ -21,23 +20,23 @@ def optimize_fuel():
 
     prob = LpProblem("Fuel_Cost_Minimization", LpMinimize)
     F = [LpVariable(f"F_{i}", lowBound=0, upBound=fuel_limits[i]) for i in range(n)]
-    prob += lpSum([F[i] * fuel_prices[i] for i in range(n)]), "Total_Fuel_Cost"
-    prob += lpSum([F[i] for i in range(n)]) >= D, "Meet_Demand"
-    prob += lpSum([F[i] for i in range(n)]) <= T, "Total_Fuel_Limit"
 
+    prob += lpSum([F[i] * fuel_prices[i] for i in range(n)])
+    prob += lpSum(F) >= D
+    prob += lpSum(F) <= T
     prob.solve()
 
     results = {}
     total_cost = 0
     total_fuel_used = 0
     for i in range(n):
-        fuel_allocated = F[i].varValue or 0
-        cost = fuel_allocated * fuel_prices[i]
-        results[f"Machine_{i + 1}"] = {
-            "fuel_allocated": round(fuel_allocated, 2),
+        allocated = F[i].varValue or 0
+        cost = allocated * fuel_prices[i]
+        results[f"Machine {i+1}"] = {
+            "fuel_allocated": round(allocated, 2),
             "cost": round(cost, 2)
         }
-        total_fuel_used += fuel_allocated
+        total_fuel_used += allocated
         total_cost += cost
 
     return jsonify({
